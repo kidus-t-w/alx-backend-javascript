de38687f81f468405166a6a5e64723ac87ba204f
kidus-t-w <kidusk69@gmail.com>
2024-04-08 18:14:14 +0300

    Finished task

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -10,9 +10,10 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
   //   budget[`capita-${getCurrentYear()}`] = capita;
   //   return budget;
 
-  return {
+  const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
   };
+  return budget;
 }

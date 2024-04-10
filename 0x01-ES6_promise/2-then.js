e62c74107ec70afa6f2c53e2e0bc6127d239c7e7
kidus-t-w <kidusk69@gmail.com>
2024-04-10 13:59:32 +0300

    Fixed: Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-    } else {
-      reject(new Error(''));
-    }
-    console.log('Got a response from the API');
-  });
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => Error(''));
 }

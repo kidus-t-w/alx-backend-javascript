8cbe2405989b6d900fa2323bcfbbd459424801b1
kidus-t-w <kidusk69@gmail.com>
2024-04-10 12:57:11 +0300

    removed files

--- 0x01-ES6_promise/5-photo-reject.js
+++
@@ -1,5 +0,0 @@
-export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
-}
--- 0x01-ES6_promise/6-final-user.js
+++
@@ -1,7 +0,0 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(fileName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises);
-}

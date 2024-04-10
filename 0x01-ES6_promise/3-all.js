dc64236516cac8567bd928dde5f197acef9b6055
kidus-t-w <kidusk69@gmail.com>
2024-04-10 15:21:10 +0300

    Finished Task

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((value) => {
-    console.log(value[0].body, value[1].firstName, value[1].lastName);
-  });
+  const upload = uploadPhoto();
+  const create = createUser();
+
+  return Promise.all([upload, create])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => console.log('Signup system offline'));
 }

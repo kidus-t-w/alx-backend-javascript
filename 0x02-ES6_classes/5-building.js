b5b7242a95860f58a93f77d7dd74c6a7f93e83ba
kidus-t-w <kidusk69@gmail.com>
2024-04-11 13:27:16 +0300

    Fixed: 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    try {
-      this.evacuationWarningMessage();
-    } catch (error) {
-        throw error;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
   }
 
@@ -18,11 +20,5 @@ export default class Building {
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
-    );
   }
 }

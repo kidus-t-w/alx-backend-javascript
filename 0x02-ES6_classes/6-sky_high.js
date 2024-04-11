270a8e9d859cbc047da60cf221ebfa90a4f7532e
kidus-t-w <kidusk69@gmail.com>
2024-04-11 13:36:45 +0300

    Finished Task

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set floor(newFloor) {
+    if (typeof newFloor === 'number') {
+      this._floor = newFloor;
+    }
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}

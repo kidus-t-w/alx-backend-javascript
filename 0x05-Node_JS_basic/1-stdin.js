0770b6dbfd2e58a66f77a443c75f356293850e58
kidus-t-w <kidusk69@gmail.com>
2024-06-03 13:35:33 +0300

    Finished task 1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const process = require('process');
+
+process.stdin.setEncoding('utf-8');
+process.stdout.write('Welcome to Holberton School, what is your name? \n What is your name ');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing \n');
+});

a508fb23e7d4b5fbd4b2869e239fca20b5c750f1
kidus-t-w <kidusk69@gmail.com>
2024-06-04 11:32:09 +0300

    Modified Output string

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
-  if (chunk !== null) {
+
+  if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing \n');
+  process.stdout.write('This important software is now closing\n');
 });

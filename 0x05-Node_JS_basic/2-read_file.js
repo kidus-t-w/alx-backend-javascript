10e7bc37ac4457af3df9e2e53f7c8af106fffb13
kidus-t-w <kidusk69@gmail.com>
2024-06-04 13:42:55 +0300

    Modified in to elint formating style

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -17,9 +17,9 @@ const countStudents = (dataPath) => {
     .trim()
     .split('\n');
   const studentGroups = {};
-  const dbFieldNames = fileLines[0].split(','); /* firstname,lastname,age,field */
+  const dbFieldNames = fileLines[0].split(',');
 
-  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1); /* firstname,lastname,age */
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
     const studentRecord = line.split(',');
@@ -36,13 +36,13 @@ const countStudents = (dataPath) => {
   }
 
   const totalStudents = Object.values(studentGroups).reduce(
-    (pre, cur) => (pre || []).length + cur.length
+    (pre, cur) => (pre || []).length + cur.length,
   );
   console.log(`Number of students: ${totalStudents}`);
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(
-      `Number of students in ${field}: ${group.length}. List: ${studentNames}`
+      `Number of students in ${field}: ${group.length}. List: ${studentNames}`,
     );
   }
 };

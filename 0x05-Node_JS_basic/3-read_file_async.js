f3ee95190a33144055162da1d451a0bf1ce78ffb
kidus-t-w <kidusk69@gmail.com>
2024-06-04 15:16:20 +0300

    Modified in to elint format

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -11,33 +11,33 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
       reject(new Error('Cannot load the database'));
     }
     if (data) {
-      const fileLines = data
+      const rows = data
         .toString('utf-8')
         .trim()
         .split('\n');
-      const studentGroups = {};
-      const dbFieldNames = fileLines[0].split(',');
+      const studentGroup = {};
+      const dbFieldNames = rows[0].split(',');
       const studentPropNames = dbFieldNames
         .slice(0, dbFieldNames.length - 1);
 
-      for (const line of fileLines.slice(1)) {
+      for (const line of rows.slice(1)) {
         const studentRecord = line.split(',');
         const studentPropValues = studentRecord
           .slice(0, studentRecord.length - 1);
         const field = studentRecord[studentRecord.length - 1];
-        if (!Object.keys(studentGroups).includes(field)) {
-          studentGroups[field] = [];
+        if (!Object.keys(studentGroup).includes(field)) {
+          studentGroup[field] = [];
         }
         const studentEntries = studentPropNames
           .map((propName, idx) => [propName, studentPropValues[idx]]);
-        studentGroups[field].push(Object.fromEntries(studentEntries));
+        studentGroup[field].push(Object.fromEntries(studentEntries));
       }
 
       const totalStudents = Object
-        .values(studentGroups)
+        .values(studentGroup)
         .reduce((pre, cur) => (pre || []).length + cur.length);
       console.log(`Number of students: ${totalStudents}`);
-      for (const [field, group] of Object.entries(studentGroups)) {
+      for (const [field, group] of Object.entries(studentGroup)) {
         const studentNames = group.map((student) => student.firstname).join(', ');
         console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
       }

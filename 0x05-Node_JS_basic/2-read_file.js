ec49de971e579c21a644b4de1b51de14d6599374
kidus-t-w <kidusk69@gmail.com>
2024-06-04 12:12:48 +0300

    Modified function

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 /**
  * Reads a CSV file and counts the number of students in each field.
  *
@@ -6,39 +6,40 @@ const fs = require('fs')
  * @return {void} This function does not return anything.
  */
 const countStudents = (path) => {
-    // const rowObjects = [];
-    try{
-        data = fs.readFileSync(path, 'utf-8');
-    } catch (err) {
-            console.log('Cannot load the database')
-            return
-        }
-        const row = data.split('\n')
-        const headers = row[0].split(',')
-        let cs_count = 0
-        let cs_names = []
-        let swe_count = 0
-        let swe_names = []
-        for (let i = 1; i < row.length; i++) {
-            const columns = row[i].split(',');
-            const rowObject = {};
-            for (let j = 0; j < headers.length; j++) {
-                rowObject[headers[j]] = columns[j];
-            }
-            // rowObjects.push(rowObject)
-            if (rowObject['field'] === 'CS'){
-                cs_names.push(rowObject['firstname'])
-                cs_count += 1
-            }
-            if (rowObject['field'] === 'SWE'){
-                swe_names.push(rowObject['firstname'])
-                swe_count += 1
-            }
-        }
-        console.log(`Number of students: ${row.length -2}.`)
-        console.log(`Number of students in CS: ${cs_count}. List: ${cs_names}`)
-        console.log(`Number of students in SWE: ${swe_count}. List: ${swe_names}`)
-        // console.log(rowObjects)
-}
+  // const rowObjects = [];
+  let data;
+  try {
+    data = fs.readFileSync(path, 'utf-8');
+  } catch (err) {
+    console.log('Cannot load the database');
+    return;
+  }
+  const row = data.split('\n');
+  const headers = row[0].split(',');
+  let csCount = 0;
+  const csNames = [];
+  let sweCount = 0;
+  const sweNames = [];
+  for (let i = 1; i < row.length; i += 1) {
+    const columns = row[i].split(',');
+    const rowObject = {};
+    for (let j = 0; j < headers.length; j += 1) {
+      rowObject[headers[j]] = columns[j];
+    }
+    // rowObjects.push(rowObject)
+    if (rowObject.field === 'CS') {
+      csNames.push(rowObject.firstname);
+      csCount += 1;
+    }
+    if (rowObject.field === 'SWE') {
+      sweNames.push(rowObject.firstname);
+      sweCount += 1;
+    }
+  }
+  console.log(`Number of students: ${row.length - 2}.`);
+  console.log(`Number of students in CS: ${csCount}. List: ${csNames}`);
+  console.log(`Number of students in SWE: ${sweCount}. List: ${sweNames}`);
+  // console.log(rowObjects)
+};
 
-module.exports = countStudents
+module.exports = countStudents;

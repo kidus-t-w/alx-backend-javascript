51fbfab4d967017fbf5e2d2761a2ebc95c8a9862
kidus-t-w <kidusk69@gmail.com>
2024-06-04 15:56:46 +0300

    Modified app function

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,22 +1,20 @@
 const http = require('http');
 
-const hostname = 'localhost';
-const port = 1245;
+const PORT = 1245;
+const HOST = 'localhost';
 const app = http.createServer();
-/**
- * Starts a small HTTP server using the http module.
- */
+
 app.on('request', (_, res) => {
-  const responseText = 'Hello Holberton School';
+  const responseText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Type', responseText.length);
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });
 
-app.listen(port, hostname, () => {
-  process.stdout.write(`Server listening at -> hhtp://${hostname}:${port}\n`);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;

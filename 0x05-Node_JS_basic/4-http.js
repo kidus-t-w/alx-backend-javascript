4eba32ea20f7e6a07097b706ac8c4810e00f296d
kidus-t-w <kidusk69@gmail.com>
2024-06-04 15:32:03 +0300

    Added 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,7 +7,7 @@ const port = 1245;
  */
 const app = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'test/plain');
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Hello Holberton School!');
 });
 

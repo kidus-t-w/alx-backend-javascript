d0044028dbf97c339ccc0ec4c26181f239ba582d
kidus-t-w <kidusk69@gmail.com>
2024-06-04 11:26:19 +0300

    Modified Output string

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 const process = require('process');
 
 process.stdin.setEncoding('utf-8');
-process.stdout.write('Welcome to Holberton School, what is your name?\nWhat is your name ');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {

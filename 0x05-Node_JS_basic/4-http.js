061ec4c9f349aeeb9374ad1d22e4875426a10f6d
kidus-t-w <kidusk69@gmail.com>
2024-06-04 15:29:26 +0300

    Added 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const { createServer } = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+/**
+ * Starts a small HTTP server using the http module.
+ */
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'test/plain');
+  res.end('Hello Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  // console.log(`Hello Holberton School!`)
+});

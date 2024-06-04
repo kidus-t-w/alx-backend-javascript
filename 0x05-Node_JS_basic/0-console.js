d5f1c3f3738970a42e9609fde771354a07297e6f
kidus-t-w <kidusk69@gmail.com>
2024-06-04 11:11:53 +0300

    Added documentation

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 const process = require('process');
 
+/**
+ * Writes the given message to the standard output.
+ *
+ * @param {string} message - The message to be displayed.
+ * @return {void} This function does not return anything.
+ */
 const displayMessage = (message) => {
   process.stdout.write(`${message}\n`);
 };
